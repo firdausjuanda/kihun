87861071ff9a0503cbeada7ff76c72d174194ee7
Firdaus Juanda <firdausjuanda@hotmail.com>
2022-03-13 23:25:34 +0700

    create API controller

---
+++ application/controllers/Api.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Api extends CI_Controller {
+
+	public function getUsers()
+	{
+		$this->db->select('*');
+		$this->db->from('users');
+		$res = $this->db->get()->result_array();
+        echo json_encode($res);
+	}
+}
